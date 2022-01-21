64662becf00fa5f81df1e3e35b5b960a1c1528f3
jorge segui ruiz <jorgecosty8@gmail.com>
2022-01-21 23:24:24 +0100

    validacion formulario

--- src/views/OfertaForm.vue
+++ src/views/OfertaForm.vue
@@ -1,99 +1,148 @@
 <template>
-  <form
-    @submit.prevent="addOffer(newOffer.empresa)"
-    @reset="cancelar"
-    role="form"
-  >
-    <fieldset>
-      <legend>{{ this.editando ? "Editar" : "Nueva" }} oferta</legend>
-      <!-- Aquí los inputs y botones del form -->
-      <div v-if="this.editando" class="form-group">
-        <label for="newoffer-id" id="newoffer-id-label">Id: </label><br />
-        <input
-          type="number"
-          id="newoffer-id-input"
-          name="newoffer-id"
-          disabled
-          v-model.number="newOffer.id"
-          class="form-control"
-        /><br />
-      </div>
-      <div class="form-group">
-        <label for="newoffer-empresa">Empresa: </label><br />
-        <select
-          name="newoffer-empresa"
-          v-model="newOffer.empresa"
-          class="form-control"
-        >
-          <option selected>--- Selecciona empresa ---</option>
-          <option
-            v-for="(empresa, key) in empresas"
-            :key="key"
-            :value="empresa.id"
-          >
-            {{ empresa.nombre }}
-          </option></select
-        ><br />
-      </div>
-      <div class="form-group">
-        <label for="newoffer-nombre">Nombre: </label><br />
-        <input
-          type="text"
-          id="newoffer-nombre"
-          name="newoffer-nombre"
-          v-model="newOffer.nombre"
-          class="form-control"
-        /><br />
-      </div>
-      <div class="form-group">
-        <label for="newoffer-contrato" id="newoffer-contrato-label"
-          >Contrato: </label
-        ><br />
-        <input
-          type="text"
-          id="newoffer-contrato-input"
-          name="newoffer-contrato"
-          v-model="newOffer.contrato"
-          class="form-control"
-        /><br />
-      </div>
-      <div class="form-group">
-        <label for="newoffer-contacto" id="newoffer-contacto-label"
-          >Contacto: </label
-        ><br />
-        <input
-          type="text"
-          id="newoffer-contacto-input"
-          name="newoffer-contacto"
-          v-model="newOffer.contacto"
-          class="form-control"
-        /><br />
-      </div>
-      <div class="form-group">
-        <label for="newoffer-contrato" id="newoffer-contrato-label"
-          >Email:</label
-        ><br />
-        <input
-          type="email"
-          id="newoffer-email-input"
-          name="newoffer-email"
-          v-model="newOffer.email"
-          class="form-control"
-        /><br />
-      </div>
-      <button type="submit" class="btn btn-default btn-primary">
-        {{ this.editando ? "Editar" : "Añadir" }}
-      </button>
-      <button type="reset" class="btn btn-danger">Cancelar</button>
-    </fieldset>
-  </form>
+  <div>
+    <h1>{{ editando ? "Editar" : "Nueva" }} oferta</h1>
+    <validation-observer v-slot="{ handleSubmit }">
+      <form
+        novalidate
+        @submit.prevent="handleSubmit(addOffer)"
+        @reset="cancelar"
+        role="form"
+      >
+        <fieldset>
+          
+          <!-- Aquí los inputs y botones del form -->
+          <div v-if="editando" class="form-group">
+            <label for="newoffer-id" id="newoffer-id-label">Id: </label><br />
+            <input
+              type="number"
+              id="newoffer-id-input"
+              name="newoffer-id"
+              disabled
+              v-model.number="newOffer.id"
+              class="form-control"
+            /><br />
+          </div>
+          
+
+          <div class="form-group">
+            <validation-provider v-slot="{ errors }" rules="required">
+              <label for="newoffer-empresa">Empresa: </label><br />
+              <select
+                name="newoffer-empresa"
+                v-model="newOffer.empresa"
+                class="form-control"
+                required
+              >
+                <option selected>--- Selecciona empresa ---</option>
+                <option
+                  v-for="(empresa, key) in empresas"
+                  :key="key"
+                  :value="empresa.id"
+                >
+                  {{ empresa.nombre }}
+                </option></select
+              ><span>{{ errors[0] }}</span>
+              <br />
+            </validation-provider>
+          </div>
+
+          <div class="form-group">
+            <validation-provider
+              v-slot="{ errors }"
+              rules="required|min:10|max:50"
+            >
+              <label for="newoffer-nombre">Nombre: </label><br />
+              <input
+                type="text"
+                id="newoffer-nombre"
+                name="newoffer-nombre"
+                v-model="newOffer.nombre"
+                class="form-control"
+                autofocus
+                minLength="10"
+                maxLength="50"
+                required
+              /><span>{{ errors[0] }}</span
+              ><br />
+            </validation-provider>
+          </div>
+
+          <div class="form-group">
+            <validation-provider v-slot="{ errors }" rules="required">
+              <label for="newoffer-contrato" id="newoffer-contrato-label"
+                >Contrato: </label
+              ><br />
+              <input
+                type="text"
+                id="newoffer-contrato-input"
+                name="newoffer-contrato"
+                v-model="newOffer.contrato"
+                class="form-control"
+              /><span>{{ errors[0] }}</span
+              ><br />
+            </validation-provider>
+          </div>
+
+          <div class="form-group">
+            <validation-provider v-slot="{ errors }" rules="required">
+              <label for="newoffer-contacto" id="newoffer-contacto-label"
+                >Contacto: </label
+              ><br />
+              <input
+                type="text"
+                id="newoffer-contacto-input"
+                name="newoffer-contacto"
+                v-model="newOffer.contacto"
+                class="form-control"
+                required
+              /><span>{{ errors[0] }}</span
+              ><br />
+            </validation-provider>
+          </div>
+
+          <div class="form-group">
+            <validation-provider v-slot="{ errors }" rules="required|email">
+              <label for="newoffer-contrato" id="newoffer-contrato-label"
+                >Email:</label
+              ><br />
+              <input
+                type="email"
+                id="newoffer-email-input"
+                name="newoffer-email"
+                v-model="newOffer.email"
+                class="form-control"
+                required
+              /><span>{{ errors[0] }}</span
+              ><br />
+            </validation-provider>
+          </div>
+          <button type="submit" class="btn btn-default btn-primary">
+            {{ editando ? "Editar" : "Añadir" }}
+          </button>
+          <button type="reset" class="btn btn-danger">Cancelar</button>
+        </fieldset>
+      </form>
+    </validation-observer>
+  </div>  
 </template>
 
 <script>
+import { ValidationProvider, ValidationObserver } from "vee-validate";
+import { extend } from "vee-validate";
+import { required, min, max, email } from "vee-validate/dist/rules";
+extend("required", required);
+extend("min", min);
+extend("max", max);
+extend("email", email);
+
 import api from "../service/API";
 export default {
   name: "offer-form",
   props: ["id"],
+  components: {
+    ValidationProvider,
+    ValidationObserver
+  },
   data() {
     return {
       newOffer: {
@@ -113,13 +162,13 @@ export default {
     },
   },
   methods: {
-    addOffer(id) {
+    addOffer() {
       if (this.id) {
         api.ofertas
           .modify(this.newOffer)
           .then(() => {
+            this.$router.push("/ofertas/" + this.newOffer.empresa);
             this.newOffer = {};
-            this.$router.push("/ofertas/" + id);
           })
           .catch((error) =>
             console.error(
@@ -130,8 +179,8 @@ export default {
         api.ofertas
           .create(this.newOffer)
           .then(() => {
+            this.$router.push("/ofertas/" + this.newOffer.empresa);
             this.newOffer = {};
-            this.$router.push("/ofertas/" + id);
           })
           .catch((error) =>
             console.error(
